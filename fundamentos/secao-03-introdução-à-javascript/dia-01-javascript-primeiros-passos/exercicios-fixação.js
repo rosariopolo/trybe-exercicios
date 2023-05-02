219591772ad6dd7b06124f23dc6f70b52341e655
Rosario Polo <rosario_polo_@live.com>
2023-05-02 20:35:34 -0300

    Até o exercicio 7 completo

--- fundamentos/secao-03-introdução-à-javascript/dia-01-javascript-primeiros-passos/exercicios-fixação.js
+++ fundamentos/secao-03-introdução-à-javascript/dia-01-javascript-primeiros-passos/exercicios-fixação.js
@@ -112,4 +112,36 @@ switch(pecaDeXadrez.toLowerCase()){
         console.log ("Erro.")
     
 }
+
+// 7 - Utilize if/else para escrever um código que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga as seguintes regras:
+
+// Porcentagem >= 90 -> A
+
+// Porcentagem >= 80 -> B
+
+// Porcentagem >= 70 -> C
+
+// Porcentagem >= 60 -> D
+
+// Porcentagem >= 50 -> E
+
+// Porcentagem < 50 -> F
+
+// O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+
+let porcentagem = 73;
+
+if (porcentagem < 0 || porcentagem > 100){
+    console.log("erro, nota incorreta")
+}else if(porcentagem >= 90){
+    console.log("A")
+}else if(porcentagem >= 80){
+    console.log("B")
+}else if(porcentagem >= 70){
+    console.log("C")
+}else if(porcentagem >= 50){
+    console.log("D")
+}else if(porcentagem < 50){
+    console.log("F")
+}
    
