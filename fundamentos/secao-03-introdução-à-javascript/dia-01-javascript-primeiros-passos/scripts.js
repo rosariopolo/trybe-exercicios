afb6cd2cfe60aa98d7c32ae7433e028fe1e3827a
Rosario Polo <rosario_polo_@live.com>
2023-05-02 12:03:09 -0300

    Exercicios de variaveis e tipos primitivos resolvido

---
+++ fundamentos/secao-03-introdução-à-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,14 @@
+let patientId = 50;
+let isEnrolled = true;
+let patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+let patientEmail = 'ana@email.com';
+
+console.log(typeof patientId)
+console.log(typeof isEnrolled)
+console.log(typeof patientInfo)
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof patientEmail)
