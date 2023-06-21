2dd4a7c790ea61b409f365a8e654a27d874d88cd
Rosario Polo <rosario_polo_@live.com>
2023-06-20 23:02:33 -0300

    array sort exercicio feito

---
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-02-high-order-functions-sort-e-map/arraysort.js
@@ -0,0 +1,17 @@
+// A nova tarefa do seu time de desenvolvimento é organizar o sistema de pessoas colaboradoras de uma rede de mercados. Para isso:
+
+// Utilize o sort para ordenar o array pela idade das pessoas em ordem crescente.
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+// people.sort((a,b) => a.age > b.age ? 1 : -1)
+// people.sort((a,b) => a.name > b.name ? 1 : -1)
+// Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente.
+people.sort((a,b) => a.age < b.age ? 1 : -1)
+
+console.log(people);
