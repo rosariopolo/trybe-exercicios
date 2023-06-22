af95213d8d21ebbaeb9e9fc12ba0e51606232096
Rosario Polo <rosario_polo_@live.com>
2023-06-22 19:57:41 -0300

    Exercicio terminado

---
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-high-order-functions-filter-e-reduce/exerciciosBonus.js
@@ -0,0 +1,106 @@
+// Todos os exercícios devem ser realizados utilizando reduce e, se necessário, use outra HOF. A informação será citada no enunciado.
+
+// 1 - Dada uma matriz, transforme em um array.
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+const flatten = () => arrays.reduce((acc, curr) => acc.concat(curr), []);
+
+  const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+
+// 2 - Crie uma string com os nomes de todas as pessoas autoras.
+// const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+  
+  const reduceNames = () => books
+  .reduce((acc, author, index) => index === books.length -1 ? 
+  `${acc}${author.author.name}.` : `${acc}${author.author.name},`, '');
+
+  
+// 3 - Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+// const expectedResult = 43;
+
+const averageAge = () => {
+  const totalBooks = books.length;
+  const sumBooks = books.reduce((acc, curr) => acc += curr.releaseYear - curr.author.birthYear, 0);
+  return sumBooks / totalBooks;
+}
+
+// 4 - Encontre o livro com o maior nome.
+// const expectedResult = {
+//   id: 1,
+//   name: 'As Crônicas de Gelo e Fogo',
+//   genre: 'Fantasia',
+//   author: {
+//     name: 'George R. R. Martin',
+//     birthYear: 1948,
+//   },
+//   releaseYear: 1991,
+// };
+
+const longestNamedBook = () => books
+  .reduce((acc, bigger) => acc.name.length > bigger.name.length ? acc : bigger);
+
